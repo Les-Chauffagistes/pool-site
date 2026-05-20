ba57de7ad366f65b4a6441ae558d4873a61799ce
swakraft <hugo.beets2003@gmail.com>
2026-05-20 16:33:21 +0200

    migrator boot strategy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 set -e
+
 export DATABASE_URL=postgresql://${DB_USER}:$(cat /run/secrets/db_password)@${DB_HOST}:5432/${DB_NAME}
+
+echo "Waiting migration flag..."
+until [ -f /migrations/done ] && \
+  [ "$(ls prisma/migrations/ | grep -v migration_lock.toml | sort | sha256sum | cut -d' ' -f1)" = "$(cat /migrations/done)" ]; do
+  echo "Migration(s) not applied, retry..."
+  sleep 2
+done
+
+echo "Migrations OK, starting..."
 exec node server.js
